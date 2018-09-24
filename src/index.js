f51c565dde6d81edf8aecc58a335aef5f5c03d0f
Roman Dan <gaddyya@gmail.com>
2018-09-24 19:51:42 +0300

    update all var to let, : (

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ module.exports = function solveEquation(equation) {
     function compareNumbers(a, b) {
         return a - b;
     }
-    var array = equation.split(" ");
-    var result = [];
-    var a = array[0];
-    var b = array[3] + array[4];
-    var c = array[7] + array[8];
-    var root_part = Math.sqrt(b * b - 4 * a * c);
-    var denom = 2 * a;
-    var x1 = Math.round((-b + root_part) / denom);
-    var x2 = Math.round((-b - root_part) / denom);
+    let array = equation.split(" ");
+    let result = [];
+    let a = array[0];
+    let b = array[3] + array[4];
+    let c = array[7] + array[8];
+    let root_part = Math.sqrt(b * b - 4 * a * c);
+    let denom = 2 * a;
+    let x1 = Math.round((-b + root_part) / denom);
+    let x2 = Math.round((-b - root_part) / denom);
     result.push(x1, x2);
     return result.sort(compareNumbers);
 }
